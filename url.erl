73345b51161855a92c8a4baf32bf6346dd356603
Astro <astro@spaceboyz.net>
2007-08-12 05:15:22 +0200

    Path parsing and serialization works

--- url.erl
+++ url.erl
@@ -1,45 +1,100 @@
 -module(url).
--export([parse/1]).
+-export([parse/1, string/1]).
 
 % -> {Proto,Host,Port,Path,Query}
 %  | {error}
 parse(S) ->
-    {Proto, S1} = parse_proto(S),
+    {Proto_, S1} = parse_proto(S),
+    Proto = case Proto_ of
+		"http" ->
+		    http;
+		"https" ->
+		    https;
+		_ ->
+		    Proto_
+	    end,
     {Host, Port_, S2} = parse_host(S1),
-    Port = case {Proto, Port} of
-	       {"http", nil} ->
+    Port = case {Proto, Port_} of
+	       {http, false} ->
 		   80;
-	       {"https", nil} ->
+	       {https, false} ->
 		   443;
 	       {_, P} ->
 		   P
 	   end,
-    {Proto, Host, Port}.
+    {Path, Query} = case S2 of
+			"" ->
+			    {"/", false};
+			_ ->
+			    parse_path(S2)
+		    end,
+    {Proto, Host, Port, Path, Query}.
 
 parse_proto([C1, C2, C3 | S]) when [C1, C2, C3] == "://" ->
     {"", S};
 parse_proto([C | S]) ->
-    io:format("parse_proto(~s)~n", [[C | S]]),
     {C2, S2} = parse_proto(S),
     {[C | C2], S2}.
 
-parse_host([C | S]) when [C] == ":" ->
-    io:format("parse_host at colon~n", []),
-    {PortS, S2} = parse_port(S),
-    io:format("parse_port -> ~w~n", [PortS]),
-    Port = list_to_integer(PortS),
-    {"", Port, S2};
-parse_host([C | S]) when [C] == "/" ->
-    io:format("parse_host at slash~n", []),
-    {"", nil, S};
 parse_host([C | S]) ->
-    io:format("parse_host(~s)~n", [[C|S]]),
-    {C2, Port, S2} = parse_host(S),
-    {[C | C2], Port, S2}.
+    case [C] of
+	":" ->
+	    {PortS, S2} = parse_port(S),
+	    Port = list_to_integer(PortS),
+	    {"", Port, S2};
+	"/" ->
+	    {"", false, S};
+	_->
+	    {C2, Port, S2} = parse_host(S),
+	    {[C | C2], Port, S2}
+    end;
+parse_host("") ->
+    {"", false, ""}.
 
-parse_port([C | S]) when [C] == "/" ->
-    {"", S};
 parse_port([C | S]) ->
     io:format("parse_port(~s)~n", [[C|S]]),
-    {C2, S2} = parse_port(S),
-    {[C | C2], S2}.
+    case [C] of
+	"/" ->
+	    {"", S};
+	_ ->
+	    {C2, S2} = parse_port(S),
+	    {[C | C2], S2}
+    end;
+parse_port([]) ->
+    {"", ""}.
+
+parse_path(S) ->
+    case S of
+	[C | S2] when [C] == "?" ->
+	    {"", S2};
+	[] ->
+	    {"", false};
+	[C | S2] ->
+	    {C2, S3} = parse_path(S2),
+	    {[C | C2], S3}
+    end.
+       
+string({Proto, Host, Port, Path, Query}) ->
+    if
+	is_atom(Proto) ->
+	    atom_to_list(Proto);
+	is_list(proto) ->
+	    Proto
+    end ++
+	"://" ++
+	Host ++
+	case {Proto, Port} of
+	    {http, 80} ->
+		"";
+	    {https, 443} ->
+		"";
+	    {_, _} ->
+		":" ++ integer_to_list(Port)
+	end ++
+	Path ++
+	case Query of
+	    false ->
+		"";
+	    _ ->
+		"?" ++ Query
+	end.
