c0b1cf1006b896ba6bd07c9c1f7722feaa0d135d
Astro <astro@spaceboyz.net>
2007-08-24 01:56:38 +0200

    dns: Fixed registering, fixed this, fixed that, removed debug statements, add progress information, add test/0

--- dns.erl
+++ dns.erl
@@ -1,22 +1,27 @@
 -module(dns).
--export([start/0, start/1, lookup/1]).
+-export([start/0, start/1, lookup/1, test/0]).
 -export([run/1, main/2, do_lookup/2]).
 
 start() ->
     start(100). % Default LookupInterval is 100ms
 
 start(LookupInterval) ->
-    spawn_link(?MODULE, run, [LookupInterval]).
+    Pid = spawn_link(?MODULE, run, [LookupInterval]),
+    case (catch register(dns, Pid)) of
+	{'EXIT', _} ->
+	    exit(Pid, kill),
+	    cannot_register;
+	_ ->
+	    ok
+    end.
 
 % Caller will receive {lookup, Name, [Addresses]}
 lookup(Name) ->
     dns ! {self(), lookup, Name}.
 
 
-
 run(LookupInterval) ->
     dbg:tpl(?MODULE,[{'_',[],[{message,{return_trace}}]}]),
-    register(dns, self()),
     % {Name, Inet4 | pending, Inet6 | pending}
     Results = ets:new(dns_results, []),
     % {do_lookup, Caller, Name, Family}
@@ -26,26 +31,21 @@ run(LookupInterval) ->
 
 
 main(LookupQueue, Results) ->
-    io:format("dns:main(~p, ~p)~n", [LookupQueue, Results]),
     receive
 
 	{Caller, lookup, Name} -> % Triggered by lookup/1
-	    io:format("~p lookup ~p~n", [Caller, Name]),
 	    case ets:lookup(Results, Name) of
 		[] -> % Haven't looked up yet
-		    io:format("Adding do_lookup ~p to LookupQueue~n", [Name]),
 		    timed_queue:add(LookupQueue, {do_lookup, Caller, Name, inet}),
 		    timed_queue:add(LookupQueue, {do_lookup, Caller, Name, inet6});
 		[{Name, Inet, Inet6}] -> % Already looked up
 		    Caller ! {lookup, Name, Inet ++ Inet6}
 	    end;
 
-	{do_lookup, Caller, Name, Family} -> % Triggered by LookupQueue
+	{do_lookup, Caller, Name, Family} = T -> % Triggered by LookupQueue
 						% It is impossible to have result already,
 						% because that would mean we're delivering delayed
 						% although the answer is already known
-	    io:format("do_lookup ~p ~p ~p~n", [Caller, Name, Family]),
-	    io:format("Results[~p] = ~p~n", [Name, ets:lookup(Results, Name)]),
 	    DoSend = case {Family, ets:lookup(Results, Name)} of
 			 {inet, []} -> true;
 			 {inet, [{Name, nil, _}]} -> true;
@@ -53,8 +53,7 @@ main(LookupQueue, Results) ->
 			 {inet6, []} -> true;
 			 {inet6, [{Name, _, nil}]} -> true;
 			 {inet6, _} -> false
-		      end,
-	    io:format("DoSend: ~p~n", [DoSend]),
+		     end,
 	    if
 		DoSend ->
 		    {Inet, Inet6} = case Family of
@@ -75,16 +74,18 @@ main(LookupQueue, Results) ->
 				    end,
 		    ets:insert(Results, {Name, Inet, Inet6}),
 		    spawn_link(?MODULE, do_lookup, [Name, Family]);
+		
+		% We seem to be already looking up that, reappend
 		true ->
-		    nil
+		    timed_queue:add(LookupQueue, T)
+		    %timed_queue:emit_next(LookupQueue)
 	    end,
 
 	    % Put back in queue
 	    timed_queue:add(LookupQueue, {do_lookup, Caller, Name, Family});
 
-	{lookup_result, Name, Family, Addresses} = A ->
+	{lookup_result, Name, Family, Addresses} ->
 	    % Insert into Results
-	    io:format("~p (~p)~n", [A, ets:lookup(Results, Name)]),
 	    case Family of
 		inet ->
 		    Inet = Addresses,
@@ -95,9 +96,8 @@ main(LookupQueue, Results) ->
 	    end,
 	    ets:insert(Results, {Name, Inet, Inet6}),
 
-		% Nothing nil or pending anymore?
+	    % Nothing nil or pending anymore?
 	    Notify = is_list(Inet) and is_list(Inet6),
-	    io:format("Notify: ~p~n", [Notify]),
 	    Dns = self(),
 	    timed_queue:filter(LookupQueue,
 			       fun({do_lookup, Caller, Name_, Family_})
@@ -111,10 +111,10 @@ main(LookupQueue, Results) ->
 				       end;
 				  ({do_lookup, _, _, _}) ->
 				       true
-			       end),
-	    io:format("Filtered~n");
+			       end);
 
 	{notify_requester, Caller, Name, Addresses} ->
+	    io:format("Notifying ~p for ~p: ~p~n", [Caller, Name, Addresses]),
 	    Caller ! {lookup, Name, Addresses}
     end,
     ?MODULE:main(LookupQueue, Results).
@@ -122,11 +122,36 @@ main(LookupQueue, Results) ->
 %%
 % Perform the actual query
 do_lookup(Name, Family) ->
-    io:format("dns:do_lookup(~p, ~p)~n", [Name, Family]),
+    io:format("Resolving ~p (~p)~n", [Name, Family]),
     Addresses = case inet:getaddrs(Name, Family) of
 		    {ok, A} ->
 			A;
 		    {error, _} ->
 			[]
 		end,
+    io:format("Resolved ~p (~p): ~p~n", [Name, Family, Addresses]),
     dns ! {lookup_result, Name, Family, Addresses}.
+
+
+test() ->
+    dbg:tpl(?MODULE,[{'_',[],[{message,{return_trace}}]}]),
+    case whereis(dns) of
+	undefined ->
+	    dns:start();
+	_ ->
+	    already_started
+    end,
+    
+    dns:lookup("spaceboyz.net"),
+    dns:lookup("www.google.com"),
+    
+    receive
+	{lookup, "spaceboyz.net", A1} ->
+	    io:format("spaceboyz.net is at ~p~n", [A1])
+    end,
+    receive
+	{lookup, "www.google.com", A2} ->
+	    io:format("google is at ~p~n", [A2])
+    end.
+
+	    
