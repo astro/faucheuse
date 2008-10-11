ee122dadcdb83b509471e217f7fb77d23c8b5ec0
Astro <astro@spaceboyz.net>
2008-10-11 02:12:11 +0200

    tagsoup_reader: references for message integrity, result tree folding fix

--- tagsoup_reader.erl
+++ tagsoup_reader.erl
@@ -5,40 +5,46 @@
 
 parse(Text) ->
     I = self(),
+    Ref = make_ref(),
     {ok, P} = tagsoup_parser:start_link(fun(Msg) ->
-						I ! Msg
+						I ! {Ref, Msg}
 					end),
     tagsoup_parser:push(P, Text),
-    I ! done,
     tagsoup_parser:stop(P),
+    I ! {Ref, done},
 
-    R = parse_listen([{root, [], []}]),
+    R = parse_listen(Ref, [{root, [], []}]),
     lists:foldl(
-      fun({root, _, Children}, Child) ->
+      fun({root, _, Children}, none) ->
+	      lists:reverse(Children);
+	 ({root, _, Children}, Child) ->
 	      lists:reverse([Child | Children]);
+	 ({T1Name, T1Attrs, T1Children}, none) ->
+	      {T1Name, T1Attrs,
+	       lists:reverse(T1Children)};
 	 ({T1Name, T1Attrs, T1Children}, Child) ->
 	      {T1Name, T1Attrs,
 	       lists:reverse([Child | T1Children])}
-      end, [], R).
+      end, none, R).
 
-parse_listen([Top | _] = Tree) ->
+parse_listen(Ref, [Top | _] = Tree) ->
     receive
-	{start_element, Name, Attributes} ->
-	    parse_listen([{Name, Attributes, []} | Tree]);
-	{end_element, Name} ->
+	{Ref, {start_element, Name, Attributes}} ->
+	    parse_listen(Ref, [{Name, Attributes, []} | Tree]);
+	{Ref, {end_element, Name}} ->
 	    Tree2 =
 		lists:foldl(fun(T1Name, [{T1Name, T1Attrs, T1Children}, {T2Name, T2Attrs, T2Children} | Tree1]) ->
 				    T1 = {T1Name, T1Attrs,
 					  lists:reverse(T1Children)},
 				    [{T2Name, T2Attrs, [T1 | T2Children]} | Tree1]
 			    end, Tree, ending_elements(Name, Tree)),
-	    parse_listen(Tree2);
-	{text, Text} ->
+	    parse_listen(Ref, Tree2);
+	{Ref, {text, Text}} ->
 	    {TName, TAttrs, TChildren} = Top,
 	    [_ | Tree1] = Tree,
 	    Tree2 = [{TName, TAttrs, [{text, Text} | TChildren]} | Tree1],
-	    parse_listen(Tree2);
-	done ->
+	    parse_listen(Ref, Tree2);
+	{Ref, done} ->
 	    Tree
     end.
 
